dc9018de6dc45177f5bb823cc2bcf70c684ad268
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-08-09 17:10:02 -0700

    WIP: adding more skeleton code, still in broken state

--- launcher.py
+++ launcher.py
@@ -6,39 +6,22 @@ from lcls_tools.superconducting.sc_linac_utils import ALL_CRYOMODULES
 
 from setup_linac import SETUP_CRYOMODULES, SetupCavity
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--cryomodule', '-cm',
-                        choices=ALL_CRYOMODULES, required=True,
-                        help=f'Cryomodule name as a string')
-    parser.add_argument('--cavity', '-cav', required=True,
-                        choices=range(1, 9), type=int,
-                        help=f'Cavity number as an int')
-    parser.add_argument('--shutdown', '-off', action="store_true",
-                        help='Turn off cavity and SSA')
-    parser.add_argument('--abort', '-abort', action="store_true",
-                        help='Abort cavity setup')
+
+def main():
+    global cavity_object
     
-    args = parser.parse_args()
-    print(args)
-    cm_name = args.cryomodule
-    cav_num = args.cavity
+    if cavity_object.script_is_running:
+        cavity_object.status_message = f"{cavity_object} script already running"
+        return
     
-    cavity_object: SetupCavity = SETUP_CRYOMODULES[cm_name].cavities[cav_num]
-    
-    if args.abort:
-        # TODO rewrite lcls tools for this
-        cavity_object.abort_flag = True
-    
-    elif args.shutdown:
+    if args.shutdown:
         cavity_object.shut_down()
     
     else:
         try:
             cavity_object.setup()
         except sc_linac_utils.CavityAbortError:
-            print(f"{cavity_object} successfully aborted")
-            # cavity_object.status_pv_obj.put(f"{cavity_object} successfully aborted")
+            cavity_object.status_message = f"{cavity_object} successfully aborted"
         
         except (sc_linac_utils.StepperError, sc_linac_utils.DetuneError,
                 sc_linac_utils.SSACalibrationError, PVInvalidError,
@@ -50,5 +33,25 @@ if __name__ == '__main__':
                 sc_linac_utils.CavityFaultError) as e:
             cavity_object.abort_flag = False
             cavity_object.steppertuner.abort_flag = False
-            print(str(e))
-            # cavity_object.status_pv_obj.put(str(e))
+            cavity_object.status_message = (str(e))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cryomodule', '-cm',
+                        choices=ALL_CRYOMODULES, required=True,
+                        help=f'Cryomodule name as a string')
+    parser.add_argument('--cavity', '-cav', required=True,
+                        choices=range(1, 9), type=int,
+                        help=f'Cavity number as an int')
+    parser.add_argument('--shutdown', '-off', action="store_true",
+                        help='Turn off cavity and SSA')
+    
+    args = parser.parse_args()
+    print(args)
+    cm_name = args.cryomodule
+    cav_num = args.cavity
+    
+    cavity_object: SetupCavity = SETUP_CRYOMODULES[cm_name].cavities[cav_num]
+    
+    main()
--- setup_linac.py
+++ setup_linac.py
@@ -1,42 +1,80 @@
 from lcls_tools.common.pyepics_tools.pyepics_utils import PV
-from lcls_tools.superconducting.scLinac import Cavity, CryoDict
-from lcls_tools.superconducting.sc_linac_utils import RF_MODE_SELAP
+from lcls_tools.superconducting.scLinac import (Cavity, CryoDict, Piezo, SSA,
+                                                StepperTuner)
+from lcls_tools.superconducting.sc_linac_utils import (RF_MODE_SELAP)
 
 
 class SetupCavity(Cavity):
     
-    def pv_addr(self, suffix: str):
+    def auto_pv_addr(self, suffix: str):
         return super().pv_addr("AUTO:" + suffix)
     
-    def __init__(self, cavityNum, rackObject):
+    def __init__(self, cavityNum, rackObject, ssaClass=SSA,
+                 stepperClass=StepperTuner, piezoClass=Piezo):
         super().__init__(cavityNum, rackObject)
         
         # TODO populate these when they exist
-        self.status_pv: str = self.pv_addr("")
-        self.progress_pv: str = self.pv_addr("")
+        self.progress_pv: str = self.auto_pv_addr("")
+        self.status_severity_pv: str = self.auto_pv_addr("")
         
         # TODO implement
-        self.abort_pv: str = self.pv_addr("")
+        self.running_pv: str = self.auto_pv_addr("")
+        self._running_pv_obj: PV = None
+        
+        # TODO implement
+        self.status_pv: str = self.auto_pv_addr("")
+        self._status_pv_obj: PV = None
+        
+        # TODO implement
+        self.abort_pv: str = self.auto_pv_addr("")
         self._abort_pv_obj: PV = None
         
-        # TODO need to implement
-        self.off_pv: str = self.pv_addr("")
-        self._off_pv_obj: PV = None
+        # TODO implement
+        self.shutoff_pv: str = self.auto_pv_addr("")
+        self._shutoff_pv_obj: PV = None
         
-        self.start_pv: str = self.pv_addr("SETUPSTRT")
+        self.start_pv: str = self.auto_pv_addr("SETUPSTRT")
         self._start_pv_obj: PV = None
         
-        self.ssa_cal_checkbox_pv: str = self.pv_addr("TURNON_SSASEL")
-        self._ssa_cal_checkbox_pv_obj: PV = None
+        self.ssa_cal_requested_pv: str = self.auto_pv_addr("TURNON_SSASEL")
+        self._ssa_cal_requested_pv_obj: PV = None
         
-        self.auto_tune_checkbox_pv: str = self.pv_addr("TURNON_TUNESEL")
-        self._auto_tune_checkbox_pv_obj: PV = None
+        self.auto_tune_requested_pv: str = self.auto_pv_addr("TURNON_TUNESEL")
+        self._auto_tune_requested_pv_obj: PV = None
         
-        self.cav_char_checkbox_pv: str = self.pv_addr("TURNON_CHARSEL")
-        self._cav_char_checkbox_pv_obj: PV = None
+        self.cav_char_requested_pv: str = self.auto_pv_addr("TURNON_CHARSEL")
+        self._cav_char_requested_pv_obj: PV = None
         
-        self.rf_ramp_checkbox_pv: str = self.pv_addr("TURNON_RAMPSEL")
-        self._rf_ramp_checkbox_pv_obj: PV = None
+        self.rf_ramp_requested_pv: str = self.auto_pv_addr("TURNON_RAMPSEL")
+        self._rf_ramp_requested_pv_obj: PV = None
+    
+    @property
+    def running_pv_obj(self):
+        if not self._running_pv_obj:
+            self._running_pv_obj = PV(self.running_pv)
+        return self._running_pv_obj
+    
+    @property
+    def script_is_running(self) -> bool:
+        return False
+        # TODO put in when implemented
+        # return self.running_pv_obj.get()
+    
+    @property
+    def status_pv_obj(self) -> PV:
+        if not self._status_pv_obj:
+            self._status_pv_obj = PV(self.status_pv)
+        return self._status_pv_obj
+    
+    @property
+    def status_message(self):
+        return self.status_pv_obj.get()
+    
+    @status_message.setter
+    def status_message(self, message):
+        print(message)
+        # TODO add in when available
+        # self.status_pv_obj.put(message)
     
     @property
     def start_pv_obj(self) -> PV:
@@ -45,16 +83,30 @@ class SetupCavity(Cavity):
         return self._start_pv_obj
     
     @property
-    def off_pv_obj(self) -> PV:
-        if not self._off_pv_obj:
-            self._off_pv_obj = PV(self.off_pv)
-        return self._off_pv_obj
+    def shutoff_pv_obj(self) -> PV:
+        if not self._shutoff_pv_obj:
+            self._shutoff_pv_obj = PV(self.shutoff_pv)
+        return self._shutoff_pv_obj
     
     @property
     def abort_pv_obj(self):
         if not self._abort_pv_obj:
             self._abort_pv_obj = PV(self.abort_pv)
         return self._abort_pv_obj
+    
+    @property
+    def abort_requested(self):
+        return self.abort_pv_obj.get()
+    
+    def clear_abort(self):
+        self.abort_pv_obj.put(False)
+    
+    def check_abort(self):
+        return super().check_abort()
+        # TODO implement when available
+        # if self.abort_requested:
+        #     self.clear_abort()
+        #     raise CavityAbortError(f"Abort requested for {self}")
     
     def trigger_setup(self):
         self.start_pv_obj.put(1)
@@ -67,65 +119,65 @@ class SetupCavity(Cavity):
         self.ssa.turn_off()
     
     def trigger_shut_down(self):
-        self.off_pv_obj.put(1)
+        self.shutoff_pv_obj.put(1)
     
     @property
-    def ssa_cal_checkbox_option(self) -> bool:
-        if not self._ssa_cal_checkbox_pv_obj:
-            self._ssa_cal_checkbox_pv_obj = PV(self.ssa_cal_checkbox_pv)
-        return bool(self._ssa_cal_checkbox_pv_obj.get())
+    def ssa_cal_requested(self) -> bool:
+        if not self._ssa_cal_requested_pv_obj:
+            self._ssa_cal_requested_pv_obj = PV(self.ssa_cal_requested_pv)
+        return bool(self._ssa_cal_requested_pv_obj.get())
     
     @property
-    def auto_tune_checkbox_option(self) -> bool:
-        if not self._auto_tune_checkbox_pv_obj:
-            self._auto_tune_checkbox_pv_obj = PV(self.auto_tune_checkbox_pv)
-        return bool(self._auto_tune_checkbox_pv_obj.get())
+    def auto_tune_requested(self) -> bool:
+        if not self._auto_tune_requested_pv_obj:
+            self._auto_tune_requested_pv_obj = PV(self.auto_tune_requested_pv)
+        return bool(self._auto_tune_requested_pv_obj.get())
     
     @property
-    def cav_char_checkbox_option(self) -> bool:
-        if not self._cav_char_checkbox_pv_obj:
-            self._cav_char_checkbox_pv_obj = PV(self.cav_char_checkbox_pv)
-        return bool(self._cav_char_checkbox_pv_obj.get())
+    def cav_char_requested(self) -> bool:
+        if not self._cav_char_requested_pv_obj:
+            self._cav_char_requested_pv_obj = PV(self.cav_char_requested_pv)
+        return bool(self._cav_char_requested_pv_obj.get())
     
     @property
-    def rf_ramp_checkbox_option(self) -> bool:
-        if not self._rf_ramp_checkbox_pv_obj:
-            self._rf_ramp_checkbox_pv_obj = PV(self.rf_ramp_checkbox_pv)
-        return bool(self._rf_ramp_checkbox_pv_obj.get())
+    def rf_ramp_requested(self) -> bool:
+        if not self._rf_ramp_requested_pv_obj:
+            self._rf_ramp_requested_pv_obj = PV(self.rf_ramp_requested_pv)
+        return bool(self._rf_ramp_requested_pv_obj.get())
     
     def setup(self):
-        print(f"Turning on {self} SSA if not on already")
+        self.status_message = f"Turning on {self} SSA if not on already"
         self.ssa.turn_on()
         
-        print(f"Resetting {self} interlocks")
+        self.status_message = f"Resetting {self} interlocks"
         self.reset_interlocks()
         
-        if self.ssa_cal_checkbox_option:
-            print(f"Running {self} SSA Calibration")
+        if self.ssa_cal_requested:
+            self.status_message = f"Running {self} SSA Calibration"
             self.turnOff()
             self.ssa.calibrate(self.ssa.drive_max)
-            print(f"{self} SSA Calibrated")
+            self.status_message = f"{self} SSA Calibrated"
         
         self.check_abort()
         
-        if self.auto_tune_checkbox_option:
-            print(f"Tuning {self} to Resonance")
+        if self.auto_tune_requested:
+            self.status_message = f"Tuning {self} to Resonance"
             self.move_to_resonance(use_sela=False)
-            print(f"{self} Tuned to Resonance")
+            self.status_message = f"{self} Tuned to Resonance"
         
         self.check_abort()
         
-        if self.cav_char_checkbox_option:
-            print(f"Running {self} Cavity Characterization")
+        if self.cav_char_requested:
+            self.status_message = f"Running {self} Cavity Characterization"
             self.characterize()
             self.calc_probe_q_pv_obj.put(1)
-            print(f"{self} Characterized")
+            self.status_message = f"{self} Characterized"
         
         self.check_abort()
         
-        if self.rf_ramp_checkbox_option:
+        if self.rf_ramp_requested:
             des_amp = self.ades
-            print(f"Ramping {self} to {des_amp}")
+            self.status_message = f"Ramping {self} to {des_amp}"
             self.piezo.enable_feedback()
             
             if (not self.is_on
@@ -139,12 +191,12 @@ class SetupCavity(Cavity):
             self.set_sela_mode()
             self.walk_amp(des_amp, 0.1)
             
-            print(f"Centering {self} piezo")
+            self.status_message = f"Centering {self} piezo"
             self.move_to_resonance(use_sela=True)
             
             self.set_selap_mode()
             
-            print(f"{self} Ramped Up to {des_amp} MV")
+            self.status_message = f"{self} Ramped Up to {des_amp} MV"
 
 
 SETUP_CRYOMODULES: CryoDict = CryoDict(cavityClass=SetupCavity)

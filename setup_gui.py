aa3cd35d7d8b49a0d537e58b1fb25903acae3ca2
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-08-15 16:43:21 -0700

    WIP: more purges, still broken

--- setup_gui.py
+++ setup_gui.py
@@ -2,7 +2,7 @@ import dataclasses
 from functools import partial
 from typing import Dict, List
 
-from PyQt5.QtCore import QThreadPool, QTimer, Qt
+from PyQt5.QtCore import QThreadPool, Qt
 from PyQt5.QtWidgets import (QCheckBox, QGridLayout, QGroupBox,
                              QHBoxLayout, QLabel, QPushButton,
                              QTabWidget, QVBoxLayout, QWidget)
@@ -33,20 +33,18 @@ class GUICavity:
     cm: str
     settings: Settings
     parent: Display
-    
+
     def __post_init__(self):
         self._cavity: SetupCavity = None
-        
-        self._ades_pv: PV = None
         self.setup_button = QPushButton(f"Set Up")
-        
+
         self.abort_button: QPushButton = QPushButton("Abort")
         self.abort_button.setStyleSheet(ERROR_STYLESHEET)
         self.abort_button.clicked.connect(self.cavity.trigger_abort)
-        
+
         self.turn_off_button: QPushButton = QPushButton(f"Turn Off")
         self.turn_off_button.clicked.connect(self.cavity.trigger_shut_down)
-        
+
         self.setup_button.clicked.connect(self.cavity.trigger_setup)
         self.aact_readback_label: PyDMLabel = PyDMLabel(init_channel=self.prefix + "AACTMEAN")
         self.aact_readback_label.alarmSensitiveBorder = True
@@ -54,7 +52,7 @@ class GUICavity:
         self.aact_readback_label.showUnits = True
         self.aact_readback_label.precisionFromPV = False
         self.aact_readback_label.precision = 2
-        
+
         # Putting this here because it otherwise gets garbage collected (?!)
         self.ades_spinbox: PyDMSpinbox = PyDMSpinbox(init_channel=self.prefix + "ADES")
         self.ades_spinbox.ctrl_limit_changed = lambda *args: None
@@ -67,31 +65,33 @@ class GUICavity:
         self.ades_spinbox.setRange(0, 21)
         self.ades_spinbox.update_format_string = partial(self.ades_spinbox.lineEdit().setToolTip,
                                                          "Press enter to execute ADES change")
-        
-        self.status_label: QLabel = QLabel("Ready for Setup")
+
+        self.status_label: PyDMLabel = PyDMLabel(init_channel=self.cavity.status_msg_pv)
         self.status_label.setAlignment(Qt.AlignHCenter)
         self.status_label.setWordWrap(True)
-        
+        self.status_label.alarmSensitiveBorder = True
+        self.status_label.alarmSensitiveContent = True
+
         self.expert_screen_button: PyDMEDMDisplayButton = PyDMEDMDisplayButton()
         self.expert_screen_button.filenames = ["$EDM/llrf/rf_srf_cavity_main.edl"]
         self.expert_screen_button.macros = self.cavity.edm_macro_string + (',' + "SELTAB=0,SELCHAR=3")
         self.expert_screen_button.setToolTip("EDM expert screens")
-    
+
     def kill_workers(self):
         self.status_label.setText(f"Sending abort request for CM{self.cm} cavity {self.number}")
         self.status_label.setStyleSheet(STATUS_STYLESHEET)
         self.cavity.abort_flag = True
         self.cavity.steppertuner.abort_flag = True
-    
+
     @property
     def cavity(self) -> SetupCavity:
         if not self._cavity:
             self._cavity = SETUP_CRYOMODULES[self.cm].cavities[self.number]
         return self._cavity
-    
+
     def launch_off_worker(self):
         self.cavity.trigger_shut_down()
-    
+
     def launch_ramp_worker(self):
         self.cavity.trigger_setup()
 
@@ -102,25 +102,25 @@ class GUICryomodule:
     name: str
     settings: Settings
     parent: Display
-    
+
     def __post_init__(self):
-        
+
         self.readback_label: PyDMLabel = PyDMLabel(init_channel=f"ACCL:L{self.linac_idx}B:{self.name}00:AACTMEANSUM")
         self.readback_label.alarmSensitiveBorder = True
         self.readback_label.alarmSensitiveContent = True
         self.readback_label.showUnits = True
         self.setup_button: QPushButton = QPushButton(f"Set Up CM{self.name}")
-        
+
         self.abort_button: QPushButton = QPushButton(f"Abort Action for CM{self.name}")
         self.abort_button.setStyleSheet(ERROR_STYLESHEET)
         self.abort_button.clicked.connect(self.trigger_cavity_aborts)
-        
+
         self.turn_off_button: QPushButton = QPushButton(f"Turn off CM{self.name}")
         self.turn_off_button.clicked.connect(self.trigger_cavity_turnoffs)
-        
+
         self.setup_button.clicked.connect(self.trigger_cavity_setups)
         self.gui_cavities: Dict[int, GUICavity] = {}
-        
+
         for cav_num in range(1, 9):
             gui_cavity = GUICavity(cav_num,
                                    f"ACCL:L{self.linac_idx}B:{self.name}{cav_num}0:",
@@ -128,15 +128,15 @@ class GUICryomodule:
                                    settings=self.settings,
                                    parent=self.parent)
             self.gui_cavities[cav_num] = gui_cavity
-    
+
     def trigger_cavity_turnoffs(self):
         for cavity_widget in self.gui_cavities.values():
             cavity_widget.cavity.trigger_shut_down()
-    
+
     def trigger_cavity_aborts(self):
         for cavity_widget in self.gui_cavities.values():
             cavity_widget.cavity.trigger_abort()
-    
+
     def trigger_cavity_setups(self):
         for cavity_widget in self.gui_cavities.values():
             cavity_widget.cavity.trigger_setup()
@@ -149,11 +149,11 @@ class Linac:
     cryomodule_names: List[str]
     settings: Settings
     parent: Display
-    
+
     def __post_init__(self):
         self.setup_button: QPushButton = QPushButton(f"Set Up {self.name}")
         self.setup_button.clicked.connect(self.launch_cm_workers)
-        
+
         self.abort_button: QPushButton = QPushButton(f"Abort Action for {self.name}")
         self.abort_button.setStyleSheet(ERROR_STYLESHEET)
         self.abort_button.clicked.connect(self.kill_cm_workers)
@@ -167,24 +167,24 @@ class Linac:
         self.cryomodules: List[GUICryomodule] = []
         self.cm_tab_widget: QTabWidget = QTabWidget()
         self.gui_cryomodules: Dict[str, GUICryomodule] = {}
-        
+
         for cm_name in self.cryomodule_names:
             self.add_cm_tab(cm_name)
-    
+
     def kill_cm_workers(self):
         for gui_cm in self.gui_cryomodules.values():
             gui_cm.trigger_cavity_aborts()
-    
+
     def launch_cm_workers(self):
         for gui_cm in self.gui_cryomodules.values():
             gui_cm.trigger_cavity_setups()
-    
+
     def add_cm_tab(self, cm_name: str):
         page: QWidget = QWidget()
         vlayout: QVBoxLayout = QVBoxLayout()
         page.setLayout(vlayout)
         self.cm_tab_widget.addTab(page, f"CM{cm_name}")
-        
+
         gui_cryomodule = GUICryomodule(linac_idx=self.idx, name=cm_name,
                                        settings=self.settings,
                                        parent=self.parent)
@@ -197,9 +197,9 @@ class Linac:
         hlayout.addWidget(gui_cryomodule.turn_off_button)
         hlayout.addWidget(gui_cryomodule.abort_button)
         hlayout.addStretch()
-        
+
         vlayout.addLayout(hlayout)
-        
+
         groupbox: QGroupBox = QGroupBox()
         all_cav_layout: QGridLayout = QGridLayout()
         groupbox.setLayout(all_cav_layout)
@@ -207,7 +207,7 @@ class Linac:
         for cav_num in range(1, 9):
             cav_groupbox: QGroupBox = QGroupBox(f"CM{cm_name} Cavity {cav_num}")
             cav_groupbox.setStyleSheet("QGroupBox { font-weight: bold; } ")
-            
+
             cav_vlayout: QVBoxLayout = QVBoxLayout()
             cav_groupbox.setLayout(cav_vlayout)
             cav_widgets = gui_cryomodule.gui_cavities[cav_num]
@@ -217,7 +217,7 @@ class Linac:
             cav_desamp_hlayout.addWidget(cav_widgets.ades_spinbox)
             cav_desamp_hlayout.addWidget(cav_widgets.aact_readback_label)
             cav_desamp_hlayout.addStretch()
-            
+
             cav_button_hlayout: QHBoxLayout = QHBoxLayout()
             cav_button_hlayout.addStretch()
             cav_button_hlayout.addWidget(cav_widgets.setup_button)
@@ -225,7 +225,7 @@ class Linac:
             cav_button_hlayout.addWidget(cav_widgets.abort_button)
             cav_button_hlayout.addWidget(cav_widgets.expert_screen_button)
             cav_button_hlayout.addStretch()
-            
+
             cav_vlayout.addLayout(cav_desamp_hlayout)
             cav_vlayout.addLayout(cav_button_hlayout)
             cav_vlayout.addWidget(cav_widgets.status_label)
@@ -237,45 +237,39 @@ class Linac:
 class SetupGUI(Display):
     def ui_filename(self):
         return 'setup_gui.ui'
-    
+
     def __init__(self, parent=None, args=None):
         super(SetupGUI, self).__init__(parent=parent, args=args)
         self.threadpool = QThreadPool()
         print(f"Max thread count: {self.threadpool.maxThreadCount()}")
-        
-        self.checkThreadTimer = QTimer(self)
-        # I think this is 1 second?
-        self.checkThreadTimer.setInterval(1000)
-        self.checkThreadTimer.timeout.connect(self.update_threadcount)
-        self.checkThreadTimer.start()
-        
+
         self.settings = Settings(ssa_cal_checkbox=self.ui.ssa_cal_checkbox,
                                  auto_tune_checkbox=self.ui.autotune_checkbox,
                                  cav_char_checkbox=self.ui.cav_char_checkbox,
                                  rf_ramp_checkbox=self.ui.rf_ramp_checkbox)
-        
+
         self.linac_widgets: List[Linac] = []
         for linac_idx in range(0, 4):
             self.linac_widgets.append(Linac(f"L{linac_idx}B", linac_idx,
                                             sc_linac_utils.LINAC_TUPLES[linac_idx][1],
                                             settings=self.settings,
                                             parent=self))
-        
+
         self.linac_widgets.insert(2, Linac("L1BHL", 1, sc_linac_utils.L1BHL,
                                            settings=self.settings, parent=self))
-        
+
         self.linac_aact_pvs: List[PV] = [PV(f"ACCL:L{i}B:1:AACTMEANSUM") for i in range(4)]
-        
+
         self.update_readback()
-        
+
         linac_tab_widget: QTabWidget = self.ui.tabWidget_linac
-        
+
         for linac in self.linac_widgets:
             page: QWidget = QWidget()
             vlayout: QVBoxLayout = QVBoxLayout()
             page.setLayout(vlayout)
             linac_tab_widget.addTab(page, linac.name)
-            
+
             hlayout: QHBoxLayout = QHBoxLayout()
             hlayout.addStretch()
             hlayout.addWidget(QLabel(f"{linac.name} Amplitude:"))
@@ -283,16 +277,13 @@ class SetupGUI(Display):
             hlayout.addWidget(linac.setup_button)
             hlayout.addWidget(linac.abort_button)
             hlayout.addStretch()
-            
+
             vlayout.addLayout(hlayout)
             vlayout.addWidget(linac.cm_tab_widget)
             camonitor(linac.aact_pv, callback=self.update_readback)
-    
+
     def update_readback(self, **kwargs):
         readback = 0
         for linac_aact_pv in self.linac_aact_pvs:
             readback += linac_aact_pv.get()
         self.ui.machine_readback_label.setText(f"{readback:.2f} MV")
-    
-    def update_threadcount(self):
-        self.ui.threadcount_label.setText(f"{self.threadpool.activeThreadCount()}")
--- setup_linac.py
+++ setup_linac.py
@@ -5,197 +5,197 @@ from lcls_tools.superconducting.sc_linac_utils import (RF_MODE_SELAP)
 
 
 class SetupCavity(Cavity):
-    
+
     def auto_pv_addr(self, suffix: str):
         return super().pv_addr("AUTO:" + suffix)
-    
+
     def __init__(self, cavityNum, rackObject, ssaClass=SSA,
                  stepperClass=StepperTuner, piezoClass=Piezo):
         super().__init__(cavityNum, rackObject)
-        
+
         # TODO populate these when they exist
-        self.progress_pv: str = self.auto_pv_addr("")
-        self.status_severity_pv: str = self.auto_pv_addr("")
-        
-        # TODO implement
-        self.running_pv: str = self.auto_pv_addr("")
-        self._running_pv_obj: PV = None
-        
+        self.progress_pv: str = self.auto_pv_addr("PROGRESS")
+        self.status_severity_pv: str = self.auto_pv_addr("STATUS.SEVR")
+
         # TODO implement
         self.status_pv: str = self.auto_pv_addr("")
         self._status_pv_obj: PV = None
-        
+
+        # TODO implement
+        self.status_msg_pv: str = self.auto_pv_addr("")
+        self._status_msg_pv_obj: PV = None
+
         # TODO implement
         self.abort_pv: str = self.auto_pv_addr("")
         self._abort_pv_obj: PV = None
-        
+
         # TODO implement
         self.shutoff_pv: str = self.auto_pv_addr("")
         self._shutoff_pv_obj: PV = None
-        
+
         self.start_pv: str = self.auto_pv_addr("SETUPSTRT")
         self._start_pv_obj: PV = None
-        
+
         self.ssa_cal_requested_pv: str = self.auto_pv_addr("TURNON_SSASEL")
         self._ssa_cal_requested_pv_obj: PV = None
-        
+
         self.auto_tune_requested_pv: str = self.auto_pv_addr("TURNON_TUNESEL")
         self._auto_tune_requested_pv_obj: PV = None
-        
+
         self.cav_char_requested_pv: str = self.auto_pv_addr("TURNON_CHARSEL")
         self._cav_char_requested_pv_obj: PV = None
-        
+
         self.rf_ramp_requested_pv: str = self.auto_pv_addr("TURNON_RAMPSEL")
         self._rf_ramp_requested_pv_obj: PV = None
-    
+
     @property
     def running_pv_obj(self):
-        if not self._running_pv_obj:
-            self._running_pv_obj = PV(self.running_pv)
-        return self._running_pv_obj
-    
+        if not self._status_pv_obj:
+            self._status_pv_obj = PV(self.status_pv)
+        return self._status_pv_obj
+
     @property
     def script_is_running(self) -> bool:
         return False
         # TODO put in when implemented
         # return self.running_pv_obj.get()
-    
+
     @property
     def status_pv_obj(self) -> PV:
-        if not self._status_pv_obj:
-            self._status_pv_obj = PV(self.status_pv)
-        return self._status_pv_obj
-    
+        if not self._status_msg_pv_obj:
+            self._status_msg_pv_obj = PV(self.status_msg_pv)
+        return self._status_msg_pv_obj
+
     @property
     def status_message(self):
         return self.status_pv_obj.get()
-    
+
     @status_message.setter
     def status_message(self, message):
         print(message)
         # TODO add in when available
         # self.status_pv_obj.put(message)
-    
+
     @property
     def start_pv_obj(self) -> PV:
         if not self._start_pv_obj:
             self._start_pv_obj = PV(self.start_pv)
         return self._start_pv_obj
-    
+
     @property
     def shutoff_pv_obj(self) -> PV:
         if not self._shutoff_pv_obj:
             self._shutoff_pv_obj = PV(self.shutoff_pv)
         return self._shutoff_pv_obj
-    
+
     @property
     def abort_pv_obj(self):
         if not self._abort_pv_obj:
             self._abort_pv_obj = PV(self.abort_pv)
         return self._abort_pv_obj
-    
+
     @property
     def abort_requested(self):
         return self.abort_pv_obj.get()
-    
+
     def clear_abort(self):
         self.abort_pv_obj.put(False)
-    
+
     def check_abort(self):
         return super().check_abort()
         # TODO implement when available
         # if self.abort_requested:
         #     self.clear_abort()
         #     raise CavityAbortError(f"Abort requested for {self}")
-    
+
     def trigger_setup(self):
         self.start_pv_obj.put(1)
-    
+
     def trigger_abort(self):
         self.abort_pv_obj.put(1)
-    
+
     def shut_down(self):
         self.turnOff()
         self.ssa.turn_off()
-    
+
     def trigger_shut_down(self):
         self.shutoff_pv_obj.put(1)
-    
+
     @property
     def ssa_cal_requested(self) -> bool:
         if not self._ssa_cal_requested_pv_obj:
             self._ssa_cal_requested_pv_obj = PV(self.ssa_cal_requested_pv)
         return bool(self._ssa_cal_requested_pv_obj.get())
-    
+
     @property
     def auto_tune_requested(self) -> bool:
         if not self._auto_tune_requested_pv_obj:
             self._auto_tune_requested_pv_obj = PV(self.auto_tune_requested_pv)
         return bool(self._auto_tune_requested_pv_obj.get())
-    
+
     @property
     def cav_char_requested(self) -> bool:
         if not self._cav_char_requested_pv_obj:
             self._cav_char_requested_pv_obj = PV(self.cav_char_requested_pv)
         return bool(self._cav_char_requested_pv_obj.get())
-    
+
     @property
     def rf_ramp_requested(self) -> bool:
         if not self._rf_ramp_requested_pv_obj:
             self._rf_ramp_requested_pv_obj = PV(self.rf_ramp_requested_pv)
         return bool(self._rf_ramp_requested_pv_obj.get())
-    
+
     def setup(self):
         self.status_message = f"Turning on {self} SSA if not on already"
         self.ssa.turn_on()
-        
+
         self.status_message = f"Resetting {self} interlocks"
         self.reset_interlocks()
-        
+
         if self.ssa_cal_requested:
             self.status_message = f"Running {self} SSA Calibration"
             self.turnOff()
             self.ssa.calibrate(self.ssa.drive_max)
             self.status_message = f"{self} SSA Calibrated"
-        
+
         self.check_abort()
-        
+
         if self.auto_tune_requested:
             self.status_message = f"Tuning {self} to Resonance"
             self.move_to_resonance(use_sela=False)
             self.status_message = f"{self} Tuned to Resonance"
-        
+
         self.check_abort()
-        
+
         if self.cav_char_requested:
             self.status_message = f"Running {self} Cavity Characterization"
             self.characterize()
             self.calc_probe_q_pv_obj.put(1)
             self.status_message = f"{self} Characterized"
-        
+
         self.check_abort()
-        
+
         if self.rf_ramp_requested:
             des_amp = self.ades
             self.status_message = f"Ramping {self} to {des_amp}"
             self.piezo.enable_feedback()
-            
+
             if (not self.is_on
                     or (self.is_on and self.rf_mode != RF_MODE_SELAP)):
                 self.ades = min(5, des_amp)
-            
+
             self.turn_on()
-            
+
             self.check_abort()
-            
+
             self.set_sela_mode()
             self.walk_amp(des_amp, 0.1)
-            
+
             self.status_message = f"Centering {self} piezo"
             self.move_to_resonance(use_sela=True)
-            
+
             self.set_selap_mode()
-            
+
             self.status_message = f"{self} Ramped Up to {des_amp} MV"
 
 
